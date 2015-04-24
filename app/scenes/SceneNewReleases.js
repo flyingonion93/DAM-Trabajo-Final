e5c96a255ab19c9879577e076f2c5db5487fad16
amnor <jrpardomas@gmail.com>
2015-04-24 16:19:52 +0200

    NewReleases_primers_pasos
    Provant merdes

--- app/scenes/SceneNewReleases.js
+++ app/scenes/SceneNewReleases.js
@@ -9,7 +9,18 @@ SceneSceneNewReleases.prototype.initialize = function () {
 	// this function will be called only once when the scene manager show this scene first time
 	// initialize the scene controls and styles, and initialize your variables here
 	// scene HTML and CSS will be loaded before this function is called
-
+	$('#ReleaseOne').sfImage({
+		src:''
+	});
+	$('#ReleaseTwo').sfImage({
+		src:''
+	});
+	$('#ReleaseThree').sfImage({
+		src:''
+	});
+	$('#MoreReleases').sfButton({
+		text:'+'
+	});
 };
 
 SceneSceneNewReleases.prototype.handleShow = function (data) {
