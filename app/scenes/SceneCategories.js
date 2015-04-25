15f847438c3eaf392806fbaab1ee0f031d1e595b
Ignasi <ignasisal@orxatastudios.com>
2015-04-25 19:29:07 +0200

    Categories

--- app/scenes/SceneCategories.js
+++ app/scenes/SceneCategories.js
@@ -19,11 +19,40 @@ SceneSceneMainSugestionsCategories.prototype.handleShow = function (data) {
 	// this function will be called when the scene manager show this scene
 	
 	scene_name = 'SceneCategories';
+	
+	alert(api_key);
+	session_id = localStorage.getItem('session_id');
+	alert(session_id);
+		
+	alert(API+"/discover/movie?api_key="+api_key);
+	$.ajax({
+	  type: "GET",
+	  crossDomain: true,
+	  async: true,
+	  dataType: "json",
+	  url: API+"/discover/movie?api_key="+api_key,
+	  success: function(data){
+		//sort( data )
+	  	alert('success');
+	  	for (var i = 2; i >= 0; i--) {
+	  		$( "#moviesCategories" ).append( '<div class=" item col-xs-4" href="'+data.results[i].id+'"><img src="'+base_url+'w342'+data.results[i].poster_path+'"/></div>' );  		
+	  		//$("#row").append('<div class="item col-xs-4" href="'+data.results[i].id+'"><img src="'+base_url+'w342'+data.results[i].poster_path+'"/></div>');
+	  	}
+	  	
+	  	$( '#moviesCategories div.item' ).keynav();
+	  },
+	  error: function(){
+	  	alert( 'error' );
+	  }
+	});
 };
 
 SceneSceneMainSugestionsCategories.prototype.handleHide = function () {
 	alert("SceneSceneMainSugestionsCategories.handleHide()");
 	// this function will be called when the scene manager hide this scene
+	$("#topCategories").html('');
+	$("#moviesCategories").html('');
+	$("#bottomCategories").html('');
 };
 
 SceneSceneMainSugestionsCategories.prototype.handleFocus = function () {
--- app/scenes/SceneSugestions.js
+++ app/scenes/SceneSugestions.js
@@ -9,6 +9,39 @@ SceneSceneSugestions.prototype.initialize = function () {
 	// this function will be called only once when the scene manager show this scene first time
 	// initialize the scene controls and styles, and initialize your variables here
 	// scene HTML and CSS will be loaded before this function is called
+	/*alert(api_key);
+	session_id = localStorage.getItem('session_id');
+	alert(session_id);
+		
+	alert(API+"/discover/movie?api_key="+api_key);
+	$.ajax({
+	  type: "GET",
+	  crossDomain: true,
+	  async: true,
+	  dataType: "json",
+	  url: API+"/discover/movie?api_key="+api_key,
+	  success: function(data){
+		//sort( data )
+	  	alert('success');
+	  	for (var i = 13; i >= 8; i--) {
+	  		$( "#moviesSugestions" ).append( '<div class=" item col-xs-4" href="'+data.results[i].id+'"><img src="'+base_url+'w342'+data.results[i].poster_path+'"/></div>' );  		
+	  		//$("#row").append('<div class="item col-xs-4" href="'+data.results[i].id+'"><img src="'+base_url+'w342'+data.results[i].poster_path+'"/></div>');
+	  	}
+	  	
+	  	$( '#moviesSugestions div.item' ).keynav();
+	  },
+	  error: function(){
+	  	alert( 'error' );
+	  }
+	});*/
+};
+
+SceneSceneSugestions.prototype.handleShow = function (data) {
+	alert("SceneSceneSugestions.handleShow()");
+	// this function will be called when the scene manager show this scene	
+
+	scene_name = 'SceneSugestions';
+	
 	alert(api_key);
 	session_id = localStorage.getItem('session_id');
 	alert(session_id);
@@ -24,8 +57,8 @@ SceneSceneSugestions.prototype.initialize = function () {
 		//sort( data )
 	  	alert('success');
 	  	for (var i = 13; i >= 8; i--) {
-	  		//$( "#moviesSugestions" ).append( '<div class=" item col-xs-4" href="'+data.results[i].id+'"><img src="'+base_url+'w342'+data.results[i].poster_path+'"/></div>' );  		
-	  		$("#row").append('<div class="item col-xs-4" href="'+data.results[i].id+'"><img src="'+base_url+'w342'+data.results[i].poster_path+'"/></div>');
+	  		$( "#moviesSugestions" ).append( '<div class=" item col-xs-4" href="'+data.results[i].id+'"><img src="'+base_url+'w342'+data.results[i].poster_path+'"/></div>' );  		
+	  		//$("#row").append('<div class="item col-xs-4" href="'+data.results[i].id+'"><img src="'+base_url+'w342'+data.results[i].poster_path+'"/></div>');
 	  	}
 	  	
 	  	$( '#moviesSugestions div.item' ).keynav();
@@ -36,16 +69,13 @@ SceneSceneSugestions.prototype.initialize = function () {
 	});
 };
 
-SceneSceneSugestions.prototype.handleShow = function (data) {
-	alert("SceneSceneSugestions.handleShow()");
-	// this function will be called when the scene manager show this scene	
-
-	scene_name = 'SceneSugestions';
-};
-
 SceneSceneSugestions.prototype.handleHide = function () {
 	alert("SceneSceneSugestions.handleHide()");
 	// this function will be called when the scene manager hide this scene
+	
+	$("#topSugestions").html('');
+	$("#moviesSugestions").html('');
+	$("#bottomSugestions").html('');
 };
 
 SceneSceneSugestions.prototype.handleFocus = function () {
