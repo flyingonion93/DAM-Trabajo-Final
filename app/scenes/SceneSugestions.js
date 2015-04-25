dad585c0c40aec79a5dd551dbd7cf235956e4203
Ignasi <ignasisal@orxatastudios.com>
2015-04-25 17:23:51 +0200

    Canvis xorra

--- app/scenes/SceneSugestions.js
+++ app/scenes/SceneSugestions.js
@@ -23,11 +23,12 @@ SceneSceneSugestions.prototype.initialize = function () {
 	  success: function(data){
 		//sort( data )
 	  	alert('success');
-	  	for (var i = 10; i >= 8; i--) {
-	  		$( "#moviesSugestions" ).append( '<div class=" item col-xs-4" href="'+data.results[i].id+'"><img src="'+base_url+'w342'+data.results[i].poster_path+'"/></div>' );  		
+	  	for (var i = 13; i >= 8; i--) {
+	  		//$( "#moviesSugestions" ).append( '<div class=" item col-xs-4" href="'+data.results[i].id+'"><img src="'+base_url+'w342'+data.results[i].poster_path+'"/></div>' );  		
+	  		$("#moviesSugestions div.row").append('<div class="item col-xs-4" href="'+data.results[i].id+'"><img src="'+base_url+'w342'+data.results[i].poster_path+'"/></div>');
 	  	}
 	  	
-	  	$( '#moviesSugestions div.item' ).keynav();
+	  	$( '#moviesSugestions div.row div.item' ).keynav();
 	  },
 	  error: function(){
 	  	alert( 'error' );
