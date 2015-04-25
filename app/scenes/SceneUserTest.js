15bf67a4f00914a8b0e705f377e65ded3a9bedc3
viferpar <vferrandizp@gmail.com>
2015-04-25 14:27:12 +0200

    UserTestNavigation fix

--- app/scenes/SceneUserTest.js
+++ app/scenes/SceneUserTest.js
@@ -9,12 +9,13 @@ SceneSceneUserTest.prototype.initialize = function () {
 	// this function will be called only once when the scene manager show this scene first time
 	// initialize the scene controls and styles, and initialize your variables here
 	// scene HTML and CSS will be loaded before this function is called
-	$( '#questions div.item' ).keynav();
+	
 };
 
 SceneSceneUserTest.prototype.handleShow = function (data) {
 	alert("SceneSceneUserTest.handleShow()");
 	// this function will be called when the scene manager show this scene
+	$( '#questions div.item' ).keynav();
 };
 
 SceneSceneUserTest.prototype.handleHide = function () {
