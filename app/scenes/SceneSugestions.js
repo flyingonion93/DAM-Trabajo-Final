7beff035470bb57896b20d525c0e0e5c7c0679a8
Ignasi <ignasisal@orxatastudios.com>
2015-04-25 11:01:56 +0200

    Afegits colorets

--- app/scenes/SceneSugestions.js
+++ app/scenes/SceneSugestions.js
@@ -23,7 +23,7 @@ SceneSceneSugestions.prototype.initialize = function () {
 	  success: function(data){
 		//sort( data )
 	  	alert('success');
-	  	for (var i = 13; i >= 8; i--) {
+	  	for (var i = 10; i >= 8; i--) {
 	  		$( "#moviesSugestions" ).append( '<div class=" item col-xs-4" href="'+data.results[i].id+'"><img src="'+base_url+'w342'+data.results[i].poster_path+'"/></div>' );  		
 	  	}
 	  	
